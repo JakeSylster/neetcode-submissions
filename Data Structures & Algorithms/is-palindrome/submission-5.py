303754effb90e8004b84458887d30005f0d47a58
Ajay Golla <44961432+JakeSylster@users.noreply.github.com>
2026-09-10 18:12:56 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        print(s[::-1])
+        fake=""
+        fake= "".join(char for char in s if char.isalnum())
+        fake=fake.lower()
+        if fake==fake[::-1]:
+            return True
+        return False
+        '''for ch in s:
+            if 97 <= ord(ch) <= 122 or 65 <= ord(ch) <= 90 or 48<= ord(ch) <= 57:
+                fake+=ch
+                print(fake)
+        fake=fake.lower()
+        if fake==fake[::-1]:
+            return True
+        return False'''
